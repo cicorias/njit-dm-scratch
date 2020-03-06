971c6a29a53e9cd79c449434d39df72de8d620cd
Shawn Cicoria <github@cicoria.com>
2020-03-05 20:14:31 -0500

    move to py files

---
+++ nb/adriver.py
@@ -0,0 +1,32 @@
+from typing import List, Tuple
+from itertools import combinations, product, permutations
+from abc import ABC, abstractmethod, abstractstaticmethod, abstractclassmethod
+import pandas as pd
+import logging
+
+
+from apriori import CollapsedCsvFileReader, Apriori
+from assoc import generate_associations
+
+logging.basicConfig()
+logger = logging.getLogger('apriori')
+logger.setLevel(logging.INFO)
+
+#fr = CollapsedCsvFileReader('./data/test4.csv')
+fr = CollapsedCsvFileReader('./data/test-dm-bookch6.csv')
+
+t2 = fr.read()
+
+#logger.info("tran count: {0}".format(t2))
+g = Apriori(t2)
+#output = g.generate_levels(support_level=0.60, drop_below_support=False)
+output = g.generate_levels(support_level=0.22, drop_below_support=True)
+
+
+print(output)
+
+
+nnn = generate_associations(output)
+
+
+print(nnn)
---
+++ nb/assoc.py
@@ -0,0 +1,36 @@
+from typing import List, Tuple
+from itertools import combinations, product, permutations
+from abc import ABC, abstractmethod, abstractstaticmethod, abstractclassmethod
+import pandas as pd
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger("apriori")
+
+
+#maybe make a data frame outbound..
+def generate_associations(data: pd.DataFrame, threshold = 0.10):
+    rv = list()
+    for r in data.iterrows():
+        # current row ID
+        idx = r[0]
+        item = data.iloc[idx]['itemsets']
+        #print("at {0}  for {1}".format(idx, item))
+        ##maybe make this a dict with key, support, and current itemset, associated itemsets
+        all_other = [ k for k,v in data.iterrows() if k != idx]
+        ##print("at {0}  for {1}".format(idx, item))
+        ##print(all_other) # all other itemsets current belongs to...
+        
+        ## THIS current item support.
+        support = data.iloc[idx]['support']
+        
+        ###rv = [ y for y in all_other ]
+        ###rv = [ (item, support, output.iloc[y]['itemsets'], output.iloc[y]['itemsets']['support']) for y in all_other ]
+        temp = [ (item, support, data.iloc[y]['itemsets'], data.iloc[y]['support']) for y in all_other ]
+        #print("type: {0}".format(type(temp)))
+        rv.extend(temp)
+        
+
+        
+    return rv
+
