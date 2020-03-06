6a5fa706f62565789b260b7303e0d17eb6353dd9
Shawn Cicoria <github@cicoria.com>
2020-03-06 14:58:35 -0500

    refactor 1

--- nb/adriver.py
+++ nb/adriver.py
@@ -1,40 +1,26 @@
-from typing import List, Tuple
-from itertools import combinations, product, permutations
-from abc import ABC, abstractmethod, abstractstaticmethod, abstractclassmethod
-import pandas as pd
 import logging
 
-
-from apriori import CollapsedCsvFileReader, Apriori
-from assoc import generate_associations
+from apriori import Apriori, CollapsedCsvFileReader
+from assoc import (calculate_confidence, generate_associations,
+                   get_support_for_key, generate_combo_itemsets, 
+                   create_associations,  myRecord)
 
 logging.basicConfig()
 logger = logging.getLogger('apriori')
 logger.setLevel(logging.INFO)
+
 
 #fr = CollapsedCsvFileReader('./data/test4.csv')
 fr = CollapsedCsvFileReader('./data/test-dm-bookch6.csv')
 
 t2 = fr.read()
 
-#logger.info("tran count: {0}".format(t2))
 g = Apriori(t2)
-#output = g.generate_levels(support_level=0.60, drop_below_support=False)
+
 output = g.generate_levels(support_level=0.22, drop_below_support=True)
 
+pc = create_associations(output)
 
-print(output)
+f = calculate_confidence(pc, confidence_level = 0.0)
 
-
-nnn = generate_associations(output)
-
-old_rows = pd.get_option("display.max_rows")
-pd.set_option("display.max_rows", 200)
-
-
-df = pd.DataFrame(nnn)
-print(df)
-
-df.to_csv('./out/out.csv')
-
-pd.set_option("display.max_rows", old_rows)
+print(f)
--- nb/assoc.py
+++ nb/assoc.py
@@ -1,12 +1,23 @@
 from typing import List, Tuple
-from itertools import combinations, product, permutations
-from abc import ABC, abstractmethod, abstractstaticmethod, abstractclassmethod
+#from itertools import combinations #, product, permutations
+#from abc import ABC, abstractmethod, abstractstaticmethod, abstractclassmethod
+from collections import namedtuple
+
 import pandas as pd
+
 import logging
 
 logging.basicConfig()
 logger = logging.getLogger("apriori")
 
+
+myRecord =  namedtuple('myRecord', ['full_key', 'antecedent', 'support1', 'result', 'support2', 'support_full_key', 'confidence'])
+
+
+def create_associations(data: pd.DataFrame):
+    n2 = generate_associations(data)
+    pc = generate_combo_itemsets(n2)
+    return pc
 
 #maybe make a data frame outbound..
 def generate_associations(data: pd.DataFrame, threshold = 0.10):
@@ -29,7 +40,49 @@ def generate_associations(data: pd.DataFrame, threshold = 0.10):
         #TODO: need another column that is the "combination" of the concatenation of the two tuples - antecedent and consequent (P(A|B))
         rv.extend(temp)
 
+    return pd.DataFrame(rv, columns= ['antecedent', 'support1', 'result', 'support2'])
+
+
+
+def generate_combo_itemsets(data: pd.DataFrame):
+    possible_combos = [ ( i[1]['antecedent'] + i[1]['result'], i[1]['antecedent'], i[1]['support1'], i[1]['result'], i[1]['support2'] ) 
+    for i in data.iterrows()
+    #elimiate where item on boths sides 
+    if not bool(set(i[1]['result']) & set(i[1]['antecedent'])) ]
+
+    return pd.DataFrame(possible_combos, columns= ['fullkey', 'antecedent', 'support1', 'result', 'support2'])
+ 
+
+
+
+def get_support_for_key(trans_list: pd.DataFrame, antecedent_key: Tuple):
+    rv = 0
+    srted_key = tuple(sorted(antecedent_key))
+    matches = trans_list[trans_list['antecedent'] == srted_key].head(1)
+    if len(matches) ==  1:
+        rv =  matches['support1'].array[0]
+    
     return rv
 
-def generate_basket_column(data: pd.DataFrame):
-    pass
+
+def calculate_confidence(data: pd.DataFrame, confidence_level = 0.0):
+    rv = list()
+    for r in data.itertuples():
+        full_key = r.fullkey
+        ant = r.antecedent
+        support1 = r.support1
+        res = r.result
+        support2 = r.support2
+        support_full_key = get_support_for_key(data, full_key)
+        if support1 != 0:
+            confidence = support_full_key / support1
+        else:
+            confidence = -1
+            
+        item_rv = myRecord(full_key, ant, support1, res, support2, support_full_key, confidence )
+        
+        rv.append( item_rv )
+        
+ 
+    rv_df = pd.DataFrame(rv)
+    return rv_df[rv_df.confidence > confidence_level] 
