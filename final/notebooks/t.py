1811f11484d8c4eb1698a7e43aa7fd62787ef72f
Shawn Cicoria <github@cicoria.com>
2020-05-01 13:46:56 -0400

    remove some files

--- final/notebooks/t.py
+++
@@ -1,7 +0,0 @@
-#this is for development only and not relevant
-from utils.helpers import *
-
-
-print(in_script())
-
-print(in_notebook())
--- final/tests/test_pyml.py
+++
@@ -1,5 +0,0 @@
-from pyml import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
